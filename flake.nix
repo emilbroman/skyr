88ce66c0e719d25f9d9f19046fdfb80356b8927e
Emil Broman <emil@emilbroman.me>
2026-03-11 00:00:13 +0100

    chore: remove lld from flake

--- flake.nix
+++ flake.nix
@@ -3,25 +3,21 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      utils,
-    }:
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
     utils.lib.eachDefaultSystem (
-      system:
-      let
+      system: let
         pkgs = nixpkgs.legacyPackages."${system}";
-      in
-      {
+      in {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             rustup
             cargo
             gnumake
             protobuf
-            lld
           ];
           shellHook = ''
             export RUSTUP_TOOLCHAIN=nightly
